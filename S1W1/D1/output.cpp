2f7008ba59994e50c1c3321ff894aa562f9b7743
Abu Al Nasir Mitul <nasirmitul28@gmail.com>
2024-05-18 23:43:26 +0600

    just tried the priting in cpp

---
+++ S1W1/D1/output.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int main()
+{
+    //printing in cpp
+    std::cout << "Hello World"<<std::endl;
+
+    //variable declaration in cpp
+    int a = 10;
+    long long int b = 100000000;
+    
+    //printing in cpp
+    std::cout << "this is the int number "<<a;
+
+    return 0;
+}
