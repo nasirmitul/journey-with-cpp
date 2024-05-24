c38c427c718f615dd98c79325eb0f4886404eeee
Abu Al Nasir Mitul <nasirmitul28@gmail.com>
2024-05-24 22:35:55 +0600

    all solved

--- S1W1/D6 - Mid/problem5.cpp
+++ S1W1/D6 - Mid/problem5.cpp
@@ -5,17 +5,43 @@ int main()
     int t;
     cin >> t;
 
-    int n, sum, temp = 0;
-
-    for (int i = 1; i <= t; i++)
+    for (int test = 1; test <= t; test++)
     {
+        int n, sum;
         cin >> n >> sum;
+
         int ar[n];
 
-        for (int j = 0; j < n; j++)
+        for (int i = 0; i < n; i++)
         {
-            
+            cin >> ar[i];
+        }
+
+        int flag = 0;
+
+        for (int i = 0; i < n - 2; i++)
+        {
+            for (int j = i + 1; j < n - 1; j++)
+            {
+                for (int k = j + 1; k < n; k++)
+                {
+                    if (ar[i] + ar[j] + ar[k] == sum)
+                    {
+                        flag = 1;
+                    }
+                }
+            }
+        }
+
+        if (flag == 1)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
         }
     }
+
     return 0;
 }
